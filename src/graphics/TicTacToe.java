7a2c561a360cc18cdb9c12da01fb2643510216e8
Dawid Layne <klatkiewiczdawid@gmail.com>
2019-03-14 11:25:14 -0400

    Added restart button to winner screen

--- src/graphics/TicTacToe.java
+++ src/graphics/TicTacToe.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import javax.swing.Timer;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JButton;
 
 import logic.Game;
 
@@ -18,6 +19,7 @@ public class TicTacToe extends JPanel {
     private List<Tile> tiles;
     private Timer timer;
     private final int SIZE = 600;
+    private final int BOARD_SIZE = 3;
 
     TicTacToe() {
 
@@ -25,11 +27,15 @@ public class TicTacToe extends JPanel {
         // going outside of the screen.
         setPreferredSize(new Dimension(SIZE, SIZE));
 
+        // initializes game
+        initGame();
+    }
+
+    protected void initGame() {
+
+        removeAll();
+
         setBackground(Color.black);
-
-        // Size of the board
-        final int BOARD_SIZE = 3;
-
         // Creates new game instance
         this.game = new Game(BOARD_SIZE, new MouseInput(SIZE / BOARD_SIZE, this));
 
@@ -63,17 +69,32 @@ public class TicTacToe extends JPanel {
         if (game.isRunning()) {
             game.update();
         } else {
+
             window.setColor(Color.red);
 
             window.setFont(new Font("TAHOMA", Font.BOLD, 40));
 
+            // make string with winner
             final String winner = "WINNER: " + game.getWinner();
 
+            // get pixel width of string
             final int w = window.getFontMetrics().stringWidth(winner);
 
+            // draw string in center
             window.drawString(winner, (SIZE / 2) - (w / 2), 50);
 
+            // stops updates
             timer.stop();
+
+            // creates new game buttons and centers it
+            JButton newGame = new JButton("Play again");
+            newGame.setBounds((SIZE / 2) - (150 / 2),100,150,45);
+
+            // when button pressed, initialize a new game.
+            newGame.addActionListener(e -> initGame());
+
+            // adds button to board
+            add(newGame);
         }
     }
 
